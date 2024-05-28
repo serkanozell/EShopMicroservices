5c3c18a5d6ce8d15092aac7393886df523980f32
serkanozel399 <serkanozel399@gmail.com>
2024-05-28 17:48:22 +0300

    Basket.API - decorator pattern, proxy pattern, cache aside pattern added for redis, dockerfile and compose modified

---
+++ src/Services/Basket/Basket.API/Data/CachedBasketRepository.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.Caching.Distributed;
+using System.Text.Json;
+
+namespace Basket.API.Data
+{
+    public class CachedBasketRepository(IBasketRepository _decorated, IDistributedCache _cache) : IBasketRepository
+    {
+        public async Task<ShoppingCart> GetBasket(string userName, CancellationToken cancellationToken = default)
+        {
+            var cachedBasket = await _cache.GetStringAsync(userName, cancellationToken);
+
+            if (!string.IsNullOrEmpty(cachedBasket))
+                return JsonSerializer.Deserialize<ShoppingCart>(cachedBasket)!;
+
+            var basket = await _decorated.GetBasket(userName, cancellationToken);
+            await _cache.SetStringAsync(userName, JsonSerializer.Serialize(basket), cancellationToken);
+
+            return basket;
+        }
+
+        public async Task<ShoppingCart> StoreBasket(ShoppingCart basket, CancellationToken cancellationToken = default)
+        {
+            await _decorated.StoreBasket(basket, cancellationToken);
+            await _cache.SetStringAsync(basket.UserName, JsonSerializer.Serialize(basket), cancellationToken);
+            return basket;
+        }
+        public async Task<bool> DeleteBasket(string userName, CancellationToken cancellationToken = default)
+        {
+            await _decorated.DeleteBasket(userName, cancellationToken);
+            await _cache.RemoveAsync(userName, cancellationToken);
+            return true;
+        }
+    }
+}
