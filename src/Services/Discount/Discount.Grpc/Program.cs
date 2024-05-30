f030454b8b2a04a3a39ee07cfa2cb893f0ec0eda
serkanozel399 <serkanozel399@gmail.com>
2024-05-31 00:49:19 +0300

    Discount Service - CRUD operations added, dockerfile and compose completed

---
+++ src/Services/Discount/Discount.Grpc/Program.cs
@@ -0,0 +1,23 @@
+using Discount.Grpc.Data;
+using Discount.Grpc.Services;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddGrpc();
+
+builder.Services.AddDbContext<DiscountContext>(options =>
+{
+    options.UseSqlite(builder.Configuration.GetConnectionString("Database"));
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+app.UseMigraiton();
+
+app.MapGrpcService<DiscountService>();
+
+app.Run();
---
+++ src/Services/Discount/Discount.Grpc/Services/DiscountService.cs
@@ -0,0 +1,70 @@
+﻿using Discount.Grpc.Data;
+using Discount.Grpc.Models;
+using Grpc.Core;
+using Mapster;
+using Microsoft.EntityFrameworkCore;
+
+namespace Discount.Grpc.Services
+{
+    public class DiscountService(DiscountContext _context, ILogger<DiscountService> _logger) : DiscountProtoService.DiscountProtoServiceBase
+    {
+        public override async Task<CouponModel> GetDiscount(GetDiscountRequest request, ServerCallContext context)
+        {
+            var coupon = await _context.Coupons.FirstOrDefaultAsync(x => x.ProductName == request.ProductName);
+            if (coupon == null)
+                coupon = new Coupon { ProductName = "No Discount", Amount = 0, Description = "No Discount Desc" };
+
+            _logger.LogInformation("Discount is retrieved for ProductName : {productName}, Amount : {amount}", coupon.ProductName, coupon.Amount);
+
+            var couponModel = coupon.Adapt<CouponModel>();
+            return couponModel;
+        }
+
+        public override async Task<CouponModel> CreateDiscount(CreateDiscountRequest request, ServerCallContext context)
+        {
+            var coupon = request.Coupon.Adapt<Coupon>();
+
+            if (coupon is null)
+                throw new RpcException(new Status(statusCode: StatusCode.InvalidArgument, "Invalid request object."));
+
+            _context.Coupons.Add(coupon);
+            await _context.SaveChangesAsync();
+
+            _logger.LogInformation("Discount is successfully created. ProductName : {ProductName}", coupon.ProductName);
+
+            var result = coupon.Adapt<CouponModel>();
+            return result;
+        }
+
+        public override async Task<CouponModel> UpdateDiscount(UpdateDiscountRequest request, ServerCallContext context)
+        {
+            var coupon = request.Coupon.Adapt<Coupon>();
+
+            if (coupon is null)
+                throw new RpcException(new Status(statusCode: StatusCode.InvalidArgument, "Invalid request object"));
+
+            _context.Coupons.Update(coupon);
+            await _context.SaveChangesAsync();
+
+            _logger.LogInformation("Discount is successfully updated. ProductName : {ProductName}", coupon.ProductName);
+
+            var result = coupon.Adapt<CouponModel>();
+            return result;
+        }
+
+        public override async Task<DeleteDiscountResponse> DeleteDiscount(DeleteDiscountRequest request, ServerCallContext context)
+        {
+            var coupon = await _context.Coupons.FirstOrDefaultAsync(x => x.ProductName == request.ProductName);
+
+            if (coupon is null)
+                throw new RpcException(new Status(statusCode: StatusCode.InvalidArgument, "Invalid request object"));
+
+            _context.Coupons.Remove(coupon);
+            await _context.SaveChangesAsync();
+
+            _logger.LogInformation("Discount is successfully deleted. ProductName : {ProductName}", coupon.ProductName);
+
+            return new DeleteDiscountResponse { Success = true };
+        }
+    }
+}
